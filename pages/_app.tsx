fb73ea9972add6cf02d44379c7a39d2020e4a761
Mert <raltahook07@gmail.com>
2023-09-28 22:38:30 +0300

    meta added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,17 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/Header/Header";
 import Layout from "@/components/Layout/Layout";
-
+import Head from "next/head";
+import dayjs from "dayjs";
+import "dayjs/locale/tr";
 export default function App({ Component, pageProps }: AppProps) {
+  dayjs.locale("tr");
   return (
     <>
+      <Head>
+        <title>{pageProps?.meta?.title}</title>
+        <meta name="description" content={pageProps?.meta?.description} />
+      </Head>
       <Header />
       <Layout>
         <Component {...pageProps} />
