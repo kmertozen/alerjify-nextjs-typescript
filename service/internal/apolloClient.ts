0b8d3c124dbfddfda67fd7b060fd24ff2c109b3a
Mert <raltahook07@gmail.com>
2023-10-04 23:25:10 +0300

    apollo client no cache removed

--- service/internal/apolloClient.ts
+++ service/internal/apolloClient.ts
@@ -3,14 +3,14 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: process.env.NEXT_GRAPHQL_ENDPOINT,
   cache: new InMemoryCache(),
-  defaultOptions: {
+  /* defaultOptions: {
     query: {
       fetchPolicy: "no-cache",
     },
     watchQuery: {
       fetchPolicy: "no-cache",
     },
-  },
+  }, */
 });
 
 export default client;
